74083d159fa3338334aa6bd9e49423093988e12f
mohagungnursalim <mohagungnursalim@gmail.com>
2022-07-16 01:07:47 +0800

    first commit

---
+++ resources/views/vendor/forum/category/index.blade.php
@@ -0,0 +1,22 @@
+{{-- $category is passed as NULL to the master layout view to prevent it from showing in the breadcrumbs --}}
+@extends ('forum::master', ['category' => null])
+
+@section ('content')
+    <div class="d-flex flex-row justify-content-between mb-2">
+        <h2 class="flex-grow-1">Semua Kategori</h2>
+    @can('admin')
+        @can ('createCategories')
+        <button type="button" class="btn btn-primary" data-open-modal="create-category">
+            {{ trans('forum::categories.create') }}
+        </button>
+
+        @include ('forum::category.modals.create')
+    @endcan
+        @endcan
+     
+    </div>
+
+    @foreach ($categories as $category)
+        @include ('forum::category.partials.list', ['titleClass' => 'lead'])
+    @endforeach
+@stop
